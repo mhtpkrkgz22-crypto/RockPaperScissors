4ff03e29243984f8683bc6c6efbb1bb4fd779909
Mehtap Kırkgöz <mhtpkrkgz22@gmail.com>
2026-01-17 21:28:50 +0300

    add humanChoise part

--- add.js
+++ add.js
@@ -12,5 +12,15 @@ function getComputerChoise(){
 
 console.log(getComputerChoise());
 
+function getHumanChoise(){
+    let choise = prompt("Write here rock, paper or scissors");
+    let select = choise.toLowerCase();
+    return select;
+}
+
+console.log(getHumanChoise());
+
+
+
 
 
