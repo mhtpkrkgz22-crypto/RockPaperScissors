996b82cb8ecd46bc996327dbfafdde86374bd81c
Mehtap Kırkgöz <mhtpkrkgz22@gmail.com>
2026-01-17 21:07:31 +0300

    add part of pick any rock, paper or scissors

--- add.js
+++ add.js
@@ -1,1 +1,16 @@
-console.log("dsljnsjk")
+function getComputerChoise(){
+    let select = Math.floor(Math.random() * 3);
+
+    if(select === 0){
+        return "rock";
+    }else if(select === 1){
+        return "paper";
+    }else{
+        return "scissors";
+    }
+}
+
+console.log(getComputerChoise());
+
+
+
