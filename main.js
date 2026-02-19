08a9ffdc5b2a85309fc39c3e2df9a8d6360c7429
Mehtap Kırkgöz <mhtpkrkgz22@gmail.com>
2026-02-19 22:49:13 +0300

    general page style finished

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ function getComputerChoice(){
 }
 
 function getHumanChoice(){
-  let choice = prompt("Write here rock, paper or scissors");
+  let choice = "";
   return choice;
 }
 
@@ -55,3 +55,40 @@ function playGame(){
 }
 
 playGame();
+
+
+
+
+/*const icons = document.querySelectorAll('#images img');
+const playBtn = document.getElementById('play-btn');
+
+let userChoice = "";
+
+// 1. İkon seçme işlemi
+icons.forEach(icon => {
+    icon.addEventListener('click', (e) => {
+        // Hepsinden silikliği kaldır, sonra seçilmeyenlere ekle
+        icons.forEach(i => i.classList.remove('not-selected'));
+        
+        userChoice = e.target.alt; // 'rock-icon' vb. bilgisini alır
+        
+        icons.forEach(i => {
+            if (i !== e.target) {
+                i.classList.add('not-selected');
+            }
+        });
+    });
+});
+
+// 2. PLAY butonuna basınca ekranın değişmesi
+playBtn.addEventListener('click', () => {
+    const container = document.getElementById('images');
+    
+    // Ekranı temizle ve yeni düzeni kur (Yumruklu düzen)
+    container.innerHTML = `
+        <img src="icons/${userChoice}.png" class="user-final">
+        <img src="icons/punch-icon.png" class="middle-punch">
+        <img src="icons/paper-icon.png" class="computer-choice"> 
+    `;
+    // Not: Buradaki paper-icon yerine JS ile rastgele seçilen icon gelecek
+});*/
