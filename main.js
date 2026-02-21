6e4007231d706929e7fa12bf15f80b993e132464
Mehtap Kırkgöz <mhtpkrkgz22@gmail.com>
2026-02-22 01:58:24 +0300

    add all icons click eventListener

--- main.js
+++ main.js
@@ -1,5 +1,55 @@
 let humanScore = 0;
 let computerScore = 0;
+let humanChoice = "";
+
+const contant = document.querySelector('#contant');
+const leftStars = document.querySelectorAll('.left');
+const rightStars = document.querySelectorAll('.right');
+const container = document.querySelector('#container');
+const images = document.querySelector('#images');
+const playBtn = document.querySelector('#play-btn');
+const resetBtn = document.querySelector('#reset-btn');
+const rock = document.querySelector('#rock-icon');
+const paper = document.querySelector('#paper-icon');
+const scissors = document.querySelector('#scissors-icon');
+
+
+function clear() {
+  
+  [rock, paper, scissors].forEach(item => {
+    item.classList.remove("active", "selected-rock", "selected-paper", "selected-scissors", "not-selected");
+  });
+}
+
+rock.addEventListener("click", (e) => {
+  clear();
+  rock.classList.add("selected-rock");
+  paper.classList.add("not-selected");
+  scissors.classList.add("not-selected");
+
+  humanChoice = e.target.alt;
+  console.log(humanChoice);
+});
+
+paper.addEventListener("click", (e) => {
+  clear();
+  paper.classList.add("selected-paper");
+  rock.classList.add("not-selected");
+  scissors.classList.add("not-selected");
+
+  humanChoice = e.target.alt;
+  console.log(humanChoice);
+});
+
+scissors.addEventListener("click", (e) => {
+  clear();
+  scissors.classList.add("selected-scissors");
+  rock.classList.add("not-selected");
+  paper.classList.add("not-selected");
+
+  humanChoice = e.target.alt;
+  console.log(humanChoice);
+});
 
 function getComputerChoice(){
   let select = Math.floor(Math.random() * 3);
@@ -19,11 +69,7 @@ function getHumanChoice(){
 }
 
 function playRound(humanChoice, computerChoice) {
-  humanChoice = humanChoice.toLowerCase();
-
-  console.log("Human choice:", humanChoice);
-  console.log("Computer choice:", computerChoice);
-
+  computerChoice = getComputerChoice();
 
   if(humanChoice === computerChoice){
     console.log("It's a tie. Both choice is: " + humanChoice);
@@ -38,23 +84,39 @@ function playRound(humanChoice, computerChoice) {
   }
 }
 
-function playGame(){
- playRound(getHumanChoice(), getComputerChoice());
- playRound(getHumanChoice(), getComputerChoice());
- playRound(getHumanChoice(), getComputerChoice());
- playRound(getHumanChoice(), getComputerChoice());
- playRound(getHumanChoice(), getComputerChoice());
- 
- console.log("Human: " + humanScore + " | Computer: " + computerScore);
 
- if(humanScore > computerScore){
-  console.log("You won! Because you are always perfect!!!")
- }else{
-  console.log("You lost. Don't be sorry. Try again!!!")
- }
-}
 
-playGame();
+playBtn.addEventListener("click", (humanChoice, computerChoice) => {
+  if(!humanChoice){
+    const alert = document.createElement('p');
+    alert.textContent = "lütfen birini seçin";
+    container.prepend(alert);
+    return;
+  }
+})
+
+
+
+
+
+/*function playGame(){
+
+
+
+  for(let i = 0; i < 5; i++) {
+    playRound(getHumanChoice(), getComputerChoice());
+
+    console.log("Human: " + humanScore + " | Computer: " + computerScore);
+
+    if(humanScore > computerScore){
+      console.log("You won! Because you are always perfect!!!");
+    }else{
+      console.log("You lost. Don't be sorry. Try again!!!");
+    }
+  }
+}*/
+
+//playGame();
 
 
 
