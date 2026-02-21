1261cb78898cf87ed2de865d45513bc7b5bf5ef9
Mehtap Kırkgöz <mhtpkrkgz22@gmail.com>
2026-02-22 02:05:58 +0300

    updated playRound function

--- main.js
+++ main.js
@@ -86,17 +86,6 @@ function playRound(humanChoice, computerChoice) {
 
 
 
-playBtn.addEventListener("click", (humanChoice, computerChoice) => {
-  if(!humanChoice){
-    const alert = document.createElement('p');
-    alert.textContent = "lütfen birini seçin";
-    container.prepend(alert);
-    return;
-  }
-})
-
-
-
 
 
 /*function playGame(){
@@ -121,36 +110,3 @@ playBtn.addEventListener("click", (humanChoice, computerChoice) => {
 
 
 
-/*const icons = document.querySelectorAll('#images img');
-const playBtn = document.getElementById('play-btn');
-
-let userChoice = "";
-
-// 1. İkon seçme işlemi
-icons.forEach(icon => {
-    icon.addEventListener('click', (e) => {
-        // Hepsinden silikliği kaldır, sonra seçilmeyenlere ekle
-        icons.forEach(i => i.classList.remove('not-selected'));
-        
-        userChoice = e.target.alt; // 'rock-icon' vb. bilgisini alır
-        
-        icons.forEach(i => {
-            if (i !== e.target) {
-                i.classList.add('not-selected');
-            }
-        });
-    });
-});
-
-// 2. PLAY butonuna basınca ekranın değişmesi
-playBtn.addEventListener('click', () => {
-    const container = document.getElementById('images');
-    
-    // Ekranı temizle ve yeni düzeni kur (Yumruklu düzen)
-    container.innerHTML = `
-        <img src="icons/${userChoice}.png" class="user-final">
-        <img src="icons/punch-icon.png" class="middle-punch">
-        <img src="icons/paper-icon.png" class="computer-choice"> 
-    `;
-    // Not: Buradaki paper-icon yerine JS ile rastgele seçilen icon gelecek
-});*/
